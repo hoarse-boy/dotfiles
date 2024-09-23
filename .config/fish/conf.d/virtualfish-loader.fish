1c5fa668c47b45e364a4f9b657206f68e5f2003a
hoarse-boy <132760273+hoarse-boy@users.noreply.github.com>
2024-09-23 20:31:21 +0700

    Delete .config/fish/conf.d directory

--- .config/fish/conf.d/virtualfish-loader.fish
+++
@@ -1,4 +0,0 @@
-set -g VIRTUALFISH_VERSION 2.5.9
-set -g VIRTUALFISH_PYTHON_EXEC /home/jho/.local/share/pipx/venvs/virtualfish/bin/python
-source /home/jho/.local/share/pipx/venvs/virtualfish/lib/python3.12/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
