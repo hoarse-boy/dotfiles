aa374e5f891ea699e2924ceb0ea5d7d3f09f2d72
jourdyGiga <jourdy@gigaming.io>
2025-02-22 22:25:36 +0700

    chore: commented these plugins and remove them

--- .config/nvim/lua/plugins/diffview-nvim.lua
+++ .config/nvim/lua/plugins/diffview-nvim.lua
@@ -1,19 +1,17 @@
-local printf = require("plugins.util.printf").printf
+return {}
+-- local printf = require("plugins.util.printf").printf
 
-return {
-  "sindrets/diffview.nvim",
-  event = "VeryLazy",
-  enabled = false, -- disabled plugin
-  -- dependencies = {},
-  -- init = function() end, -- functions are always executed during startup
-  -- opts = function(_, opts) end, -- use this to not overwrite this plugin config (usefull in lazyvim)
-  keys = {
-    -- { "<leader>l", "", desc = printf"+go.nvim" }, -- example
-    -- { "<leader>ls", "<cmd>GoFillStruct<cr>", desc = printf"Fill Struct" }, -- example
-    -- { "<s-tab>", function() require("luasnip").jump(-1) end,  mode = { "i", "s" }, desc = printf"luasnip tab", noremap = true, silent = true },
-    -- { "define keymaps", "what the keys do", desc = printf"description", mode = "vim mode" }
-  },
-  config = function()
-    require("diffview").setup()
-  end,
-}
+-- return {
+--   "sindrets/diffview.nvim",
+--   event = "VeryLazy",
+--   enabled = true,
+--   keys = {
+--     -- { "<leader>l", "", desc = printf"+go.nvim" }, -- example
+--     -- { "<leader>ls", "<cmd>GoFillStruct<cr>", desc = printf"Fill Struct" }, -- example
+--     -- { "<s-tab>", function() require("luasnip").jump(-1) end,  mode = { "i", "s" }, desc = printf"luasnip tab", noremap = true, silent = true },
+--     -- { "define keymaps", "what the keys do", desc = printf"description", mode = "vim mode" }
+--   },
+--   config = function()
+--     require("diffview").setup()
+--   end,
+-- }
--- .config/nvim/lua/plugins/hydrate.lua
+++ .config/nvim/lua/plugins/hydrate.lua
@@ -1,35 +1,35 @@
 return {
-  {
-    "stefanlogue/hydrate.nvim",
-    event = "VeryLazy",
-    enabled = false,
-    -- This installs the latest stable release.
-    -- Set to false or omit to install the latest development version
-    version = "*",
-    config = function()
-      require("hydrate").setup({
-        -- The interval between notifications in minutes
-        minute_interval = 120,
+  -- {
+  --   "stefanlogue/hydrate.nvim",
+  --   event = "VeryLazy",
+  --   enabled = false,
+  --   -- This installs the latest stable release.
+  --   -- Set to false or omit to install the latest development version
+  --   version = "*",
+  --   config = function()
+  --     require("hydrate").setup({
+  --       -- The interval between notifications in minutes
+  --       minute_interval = 120,
 
-        -- The render style for notifications
-        -- Accepted values are "default", "minimal", "simple" or "compact"
-        render_style = "compact",
+  --       -- The render style for notifications
+  --       -- Accepted values are "default", "minimal", "simple" or "compact"
+  --       render_style = "compact",
 
-        -- Loads time of last drink on startup
-        -- Useful if you don't have long-running neovim instances
-        -- or if you tend to have multiple instances running at a time
-        persist_timer = true,
+  --       -- Loads time of last drink on startup
+  --       -- Useful if you don't have long-running neovim instances
+  --       -- or if you tend to have multiple instances running at a time
+  --       persist_timer = true,
 
-        -- Sets the reminder message after "minute_interval" minutes have
-        -- passed to the the specified message
-        msg_hydrate_now = " 💧 Time for a drink ",
+  --       -- Sets the reminder message after "minute_interval" minutes have
+  --       -- passed to the the specified message
+  --       msg_hydrate_now = " 💧 Time for a drink ",
 
-        -- Sets the message after acknowledging the reminder to the
-        -- specified message
-        msg_hydrated = " 💧 You've had a drink, timer reset 💧",
-      })
-    end,
-  },
+  --       -- Sets the message after acknowledging the reminder to the
+  --       -- specified message
+  --       msg_hydrated = " 💧 You've had a drink, timer reset 💧",
+  --     })
+  --   end,
+  -- },
   -- {
   --   "folke/which-key.nvim",
   --   opts = function(_, _)
