0324b8931d56834bee960f71692235954bfffbac
jourdyGiga <jourdy@gigaming.io>
2025-03-15 12:46:05 +0700

    chore: move snacks indent to it own file
    create autocmd to not enable indent in md file

--- .config/nvim/lua/plugins/snack-nvim.lua
+++ .config/nvim/lua/plugins/snack-nvim.lua
@@ -1,6 +1,13 @@
 local my_notes_dir = "~/jho-notes"
 local printf = require("plugins.util.printf").printf
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "yaml", "yml", "toml" },
+  callback = function()
+    vim.b.snacks_indent = false
+  end,
+})
+
 -- all snacks.nvim configs goes here
 return {
   {
@@ -73,27 +80,106 @@ return {
 
       -- snacks image config at markdown.lua
       -- snacks animate config at animation.lua
-    },
 
-    bigfile = {
-      enabled = true, -- enable big file detection
-      notify = true, -- show notification when big file detected
-      size = 1.5 * 1024 * 1024, -- 1.5MB
-      line_length = 10000, -- average line length (useful for minified files)
-      -- Enable or disable features when big file detected
-      ---@param ctx {buf: number, ft:string}
-      setup = function(ctx)
-        if vim.fn.exists(":NoMatchParen") ~= 0 then
-          vim.cmd([[NoMatchParen]])
-        end
-        Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
-        vim.b.minianimate_disable = true
-        vim.schedule(function()
-          if vim.api.nvim_buf_is_valid(ctx.buf) then
-            vim.bo[ctx.buf].syntax = ctx.ft
+      -- bigfile
+      bigfile = {
+        enabled = true, -- enable big file detection
+        notify = true, -- show notification when big file detected
+        size = 1.5 * 1024 * 1024, -- 1.5MB
+        line_length = 10000, -- average line length (useful for minified files)
+        -- Enable or disable features when big file detected
+        ---@param ctx {buf: number, ft:string}
+        setup = function(ctx)
+          if vim.fn.exists(":NoMatchParen") ~= 0 then
+            vim.cmd([[NoMatchParen]])
           end
-        end)
-      end,
+          Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
+          vim.b.minianimate_disable = true
+          vim.schedule(function()
+            if vim.api.nvim_buf_is_valid(ctx.buf) then
+              vim.bo[ctx.buf].syntax = ctx.ft
+            end
+          end)
+        end,
+      },
+
+      -- indent
+      indent = {
+        -- enabled = false, -- NOTE: enable or disable snacks indent
+        ---@class snacks.indent.Config
+        indent = {
+          -- enabled = false, -- enable indent guides
+          -- char = "▎",
+          char = "│",
+          -- char = char_symbol,
+          blank = " ",
+          -- blank = "∙",
+          only_scope = false, -- only show indent guides of the scope
+          only_current = false, -- only show indent guides in the current window
+          -- hl = "SnacksIndent", ---@type string|string[] hl groups for indent guides
+          -- can be a list of hl groups to cycle through
+          hl = {
+            "Comment",
+            -- "SnacksIndent2",
+            -- "SnacksIndent3",
+            -- "SnacksIndent4",
+            -- "SnacksIndent5",
+            -- "SnacksIndent6",
+            -- "SnacksIndent7",
+            -- "SnacksIndent8",
+          },
+        },
+        ---@class snacks.indent.Scope.Config: snacks.scope.Config
+        scope = {
+          enabled = true, -- enable highlighting the current scope
+          -- animate scopes. Enabled by default for Neovim >= 0.10
+          -- Works on older versions but has to trigger redraws during animation.
+          ---@type snacks.animate.Config|{enabled?: boolean}
+          animate = {
+            enabled = vim.fn.has("nvim-0.10") == 1,
+            easing = "linear",
+            duration = {
+              step = 20, -- ms per step
+              total = 500, -- maximum duration
+            },
+          },
+          char = "│",
+          -- char = char_symbol,
+          -- underline = true, -- underline the start of the scope
+          underline = false, -- underline the start of the scope
+          -- only_current = true, -- only show scope in the current window
+          only_current = false, -- only show scope in the current window
+          hl = "Function", ---@type string|string[] hl group for scopes
+          -- hl = "SnacksIndentScope", ---@type string|string[] hl group for scopes
+        },
+        chunk = {
+          -- when enabled, scopes will be rendered as chunks, except for the
+          -- top-level scope which will be rendered as a scope.
+          enabled = true,
+          -- enabled = false,
+          -- only show chunk scopes in the current window
+          -- only_current = true,
+          only_current = false,
+          hl = "Function", ---@type string|string[] hl group for chunk scopes
+          -- hl = "SnacksIndentChunk", ---@type string|string[] hl group for chunk scopes
+          char = {
+            -- corner_top = "┌",
+            -- corner_bottom = "└",
+            corner_top = "╭",
+            -- corner_top = "╭",
+            corner_bottom = "╰",
+            horizontal = "─",
+            -- vertical = char_symbol,
+            vertical = "│",
+            arrow = ">",
+          },
+        },
+        -- filter for buffers to enable indent guides
+        -- filter = function(buf)
+        --   return vim.g.snacks_indent ~= false and vim.b[buf].snacks_indent ~= false and vim.bo[buf].buftype == ""
+        -- end,
+        priority = 200,
+      },
     },
 
     keys = {
