7b97e08da582832b0f6fd8c91dae8d3a1cea4d12
hoarse-boy <jourdymagoofficial@gmail.com>
2025-04-10 18:03:13 +0700

    feat: add new script to handle special workspace automatically

---
+++ hypr/.config/hypr/scripts/special-neovide.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# check if the Neovide quick-note window already exists
+if ! hyprctl clients -j | jq -e '.[] | select(.class == "neovide-quick-note")' >/dev/null; then
+  # if not running, launch it
+  neovide --wayland_app_id neovide-quick-note ~/jho-notes/quick-note.md &
+  
+  # Wait for the window to appear
+  for i in {1..10}; do
+    if hyprctl clients -j | jq -e '.[] | select(.class == "neovide-quick-note")' >/dev/null; then
+      break
+    fi
+    sleep 0.1
+  done
+fi
+
+# switch to the special workspace (if not already there)
+hyprctl dispatch togglespecialworkspace quick-note
