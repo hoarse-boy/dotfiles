3e049754ad22338be411e748739a6470cd1b61b9
hoarse-boy <jourdymagoofficial@gmail.com>
2025-06-02 11:54:44 +0700

    refactor: both scissor and luasnip to be in a single file snippets in plugin folder

--- nvim/.config/nvim/lua/plugins/snippets.lua
+++ nvim/.config/nvim/lua/plugins/snippets.lua
@@ -1,6 +1,38 @@
--- local chezmoi_dir = os.getenv("HOME") .. "/.local/share/chezmoi"
+local my_vscode_path = "~/.config/nvim/lua/vscode-snippets"
+local my_snipmate_path = "~/.config/nvim/lua/snippets"
+local my_lua_snippets = "~/.config/nvim/lua/luasnippets"
+local honza_snippets_path = "~/.local/share/nvim/lazy/vim-snippets/snippets" -- community driven of all programing language snippets
 
+-- must install default lazyvim's luasnip form LazyExtra
 return {
+  -- disable builtin snippet support
+  { "garymjr/nvim-snippets", enabled = false },
+
+  -- add luasnip
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = true,
+    build = (not LazyVim.is_win()) and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp" or nil,
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+      },
+    },
+    opts = function(_, opts)
+      opts.history = true
+      opts.delete_check_events = "TextChanged"
+
+      require("luasnip.loaders.from_snipmate").lazy_load({ paths = { my_snipmate_path, honza_snippets_path } })
+      require("luasnip.loaders.from_lua").lazy_load({ paths = { my_lua_snippets } })
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = my_vscode_path })
+    end,
+    keys = {},
+  },
+
+  -- update vscode style snippets
   {
     "chrisgrieser/nvim-scissors",
     event = "VeryLazy",
@@ -28,19 +60,21 @@ return {
             showHelp = "?",
           },
         },
-        telescope = {
-          -- By default, the query only searches snippet prefixes. Set this to
-          -- `true` to also search the body of the snippets.
-          alsoSearchSnippetBody = false,
+        snippetSelection = {
+          -- telescope = {
+          --   -- By default, the query only searches snippet prefixes. Set this to
+          --   -- `true` to also search the body of the snippets.
+          --   alsoSearchSnippetBody = false,
 
-          -- accepts the common telescope picker config
-          opts = {
-            layout_strategy = "horizontal",
-            layout_config = {
-              horizontal = { width = 0.9 },
-              preview_width = 0.6,
-            },
-          },
+          --   -- accepts the common telescope picker config
+          --   opts = {
+          --     layout_strategy = "horizontal",
+          --     layout_config = {
+          --       horizontal = { width = 0.9 },
+          --       preview_width = 0.6,
+          --     },
+          --   },
+          -- },
         },
 
         -- `none` writes as a minified json file using `vim.encode.json`.
