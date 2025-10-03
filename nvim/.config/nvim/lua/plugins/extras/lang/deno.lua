b4e24e77620f87edeef39e542b768410570a75bf
hoarse-boy <jourdymagoofficial@gmail.com>
2025-10-03 23:23:04 +0700

    feat: add notify when saving ts file

--- nvim/.config/nvim/lua/plugins/extras/lang/deno.lua
+++ nvim/.config/nvim/lua/plugins/extras/lang/deno.lua
@@ -1,4 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 
 autocmd("FileType", {
   pattern = "typescript",
@@ -6,6 +7,15 @@ autocmd("FileType", {
     vim.bo.tabstop = 4
     vim.bo.shiftwidth = 4
   end,
+})
+
+local format_sync_grp = augroup("tsReminderPersonal", {})
+autocmd("BufWritePre", {
+  pattern = "*.ts",
+  callback = function()
+    Snacks.notify.info("# Have you:\n- run test?\n- run lint?\n- checked todo 'FIX:' etc?", { once = false, id = 1, timeout = 5000 }) -- FIX: . check and test this. remove comments later
+  end,
+  group = format_sync_grp,
 })
 
 return {
